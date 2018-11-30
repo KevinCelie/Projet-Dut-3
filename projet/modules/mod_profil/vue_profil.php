63015300d7bd8fd8739e16e4da9a9de55c46adef
RAFAEL AURIAC <rauriac@iut.univ-paris8.fr>
2018-11-30 09:32:05 +0100

    Profil

--- projet/modules/mod_profil/vue_profil.php
+++ projet/modules/mod_profil/vue_profil.php
@@ -3,17 +3,60 @@ Class Vue_Profil{
    public function __construct () {
    }
 
-   public function affiche_liste ($array){
-   	foreach($array as $key => $val){
-   		echo $val['nom'] . "<br/>";
-   	}
-      echo "<a href='index.php?module=profil&action=formulaire'>Ajouter profil</a><br/>";
-   }
-
    public function afficheProfil($req){
-      $line = $req->fetch();
-      
-      
+      $line = $req->fetch(); 
+      echo "<div class='container' id='afficheProfil'>";
+         echo "<div class='row ProfilRow'>";
+            echo "<div class='ProfilChamp' id='NomPrenom'>";
+               echo $line['nom']."  ".$line['prenom'];
+            echo "</div>";
+         echo "</div>";
+
+         echo "<div class='row ProfilRow'>";
+            echo "<div id='ProfilAge'>";
+               echo "<div class='ProfilNomChamp'>";
+                  echo "<span>Âge :</span>";
+               echo "</div>";
+               echo "<div class='ProfilChamp'>";
+                  echo $line['age'];
+               echo "</div>";   
+            echo "</div>";
+         echo "</div>";
+
+         echo "<div class='row ProfilRow'>";
+            echo "<div id='ProfilDescription'>";
+               echo "<div class='ProfilNomChamp'>";
+                  echo "Description :";
+               echo "</div>";
+               echo "<div class='ProfilChamp'>";
+                  echo $line['description'];
+               echo "</div>";
+            echo "</div>";
+         echo "</div>";
+
+         echo "<div class='row ProfilRow'>";
+            echo "<div id='ProfilVitesseTapping'>";
+               echo "<div class='ProfilNomChamp'>";
+                  echo "Vitesse de Tapping :";
+               echo "</div>";
+               echo "<div class='ProfilChamp'>";
+                  echo $line['vitesseTapping'];
+               echo "</div>";
+            echo "</div>";
+         echo "</div>";
+
+         echo "<div class='row ProfilRow'>";
+            echo "<div id='ProfilSexe'>";
+               echo "<div class='ProfilNomChamp'>";
+                  echo "Sexe :";
+               echo "</div>";
+               echo "<div class='ProfilChamp'>";
+                  echo $line['sexe'];
+               echo "</div>";
+            echo "</div>";
+         echo "</div>";
+
+      echo "</div>" ;
    }
 
    
