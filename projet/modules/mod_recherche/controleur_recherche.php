60206534e9d37930932050f270c891f65e70bbf6
kevincelie <kcelie21@gmail.com>
2018-12-08 01:25:56 +0100

    Creation Projet et Affichage

--- projet/modules/mod_recherche/controleur_recherche.php
+++ projet/modules/mod_recherche/controleur_recherche.php
@@ -4,44 +4,45 @@ include_once 'modules/mod_recherche/modele_recherche.php';
 include_once  'modules/mod_recherche/vue_recherche.php';
 
 Class Controleur_Recherche{
-	private $vue;
-	private $modele;
+    private $vue;
+    private $modele;
 
-	public function __construct(){
-		$this -> vue = new Vue_Recherche();
-		$this -> modele = new Modele_Recherche();
-	}
+    public function __construct(){
+        $this -> vue = new Vue_Recherche();
+        $this -> modele = new Modele_Recherche();
+    }
 
-   public function affiche_recherche(){
-      $this -> vue -> afficheRecherche();
-   }
+    public function affiche_recherche(){
+        $this -> vue -> afficheRecherche();
+    }
 
-   public function affiche_page_recherche() {
-      if (isset($_POST['champRecherche'])) {
-         $req = $this -> modele -> effectuerRecherche($_POST['champRecherche']);
-         $this -> vue -> affichePageRecherche($req);
-      }
-         
-   }
+    public function affiche_page_recherche() {
+        if (isset($_POST['champRecherche'])) {
+            $profil = $this -> modele -> effectuerRechercheProfil($_POST['champRecherche']);
+            $projet = $this -> modele -> effectuerRechercheProjet($_POST['champRecherche']);
+            $this -> vue -> affichePageRecherche($profil,$projet);
+        }
 
-   // public function affiche_profil(){
-   //    $req = $this -> modele -> getProfil();
-   // 	$this -> vue -> afficheProfil($req);
-   // }
+    }
 
-   // //fonction Ajout de profil
-   // public function formulaire(){
-   //    $this -> vue -> formulaire();
-   // }
-   // public function ajout() {
+    // public function affiche_profil(){
+    //    $req = $this -> modele -> getProfil();
+    // 	$this -> vue -> afficheProfil($req);
+    // }
 
-   //    if(isset($_POST['truc']) && trim($_POST['truc']) != ''){
-   //       $nomprofil = trim(htmlspecialchars($_POST['truc']));
-   //       $this -> modele -> ajout($nomprofil);
+    // //fonction Ajout de profil
+    // public function formulaire(){
+    //    $this -> vue -> formulaire();
+    // }
+    // public function ajout() {
 
-   //    }
-   //    header('Location: index.php?module=profil&action=liste');
-   // }
+    //    if(isset($_POST['truc']) && trim($_POST['truc']) != ''){
+    //       $nomprofil = trim(htmlspecialchars($_POST['truc']));
+    //       $this -> modele -> ajout($nomprofil);
+
+    //    }
+    //    header('Location: index.php?module=profil&action=liste');
+    // }
 
 }
 ?>
